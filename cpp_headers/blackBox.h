487edde655ebc6669a39ac18760b2060c83ac8fb
Mantej Sokhi <sokhimantej99@gmail.com>
2026-08-17 12:26:32 -0700

    Added black box header file.

---
+++ cpp_headers/blackBox.h
@@ -0,0 +1,12 @@
+#ifndef BLACKBOX_H
+#define BLACKBOX_H
+
+#include"integerMath.h"
+#include"polyMath.h"
+
+using namespace std;
+
+LONG pBlackBox(const LONG &alpha,const LONG p);
+LONG getDegBB(const LONG p);
+
+#endif
